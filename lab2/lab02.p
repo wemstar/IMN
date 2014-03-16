d99be85b0ffc957dffff1fa36c5827056da15395
wemstar <sylwestermacura@gmail.com>
2014-03-16 22:10:49 +0100

    Ukończone laboratorium 2

--- lab2/lab02.p
+++ lab2/lab02.p
@@ -1,53 +1,81 @@
 set terminal jpeg
-
+#Zadanie 1
 set output 'Z01a.jpg'
-plot 'File1' u 1:2 w p title 'numerycznie','' u 1:3 w l title 'analitycznie'
+plot 'File1.txt' u 1:2 w p title 'numerycznie','' u 1:3 w l title 'analitycznie'
 
 set output 'Z01b.jpg'
-plot 'File2' u 1:2 w p title 'numerycznie','' u 1:3 w l title 'analitycznie'
+plot 'File2.txt' u 1:2 w p title 'numerycznie','' u 1:3 w l title 'analitycznie'
 
 set output 'Z01c.jpg'
-plot 'File3' u 1:2 w p title 'numerycznie','' u 1:3 w l title 'analitycznie'
+plot 'File3.txt' u 1:2 w p title 'numerycznie','' u 1:3 w l title 'analitycznie'
 
 set output 'Z01d.jpg'
-plot 'File3' u 1:4 w p title 'jawnie','File2' u 1:4 w p title 'niejawnie','File3' u 1:4 w p title 'trapezów',
+plot 'File1.txt' u 1:4 w p title 'jawnie','File2.txt' u 1:4 w p title 'niejawnie','File3.txt' u 1:4 w p title 'trapezów',
+
+#Zadanie 2
 
 set output 'Z02a.jpg'
-plot 'File4' u 1:2 w p title 'numerycznie','' u 1:3 w l title 'analitycznie'
+plot 'File4.txt' u 1:2 w p title 'numerycznie','' u 1:3 w l title 'analitycznie'
 
 set output 'Z02b.jpg'
-plot 'File5' u 1:2 w p title 'numerycznie','' u 1:3 w l title 'analitycznie'
+plot 'File5.txt' u 1:2 w p title 'numerycznie','' u 1:3 w l title 'analitycznie'
 
 set output 'Z02c.jpg'
-plot 'File6' u 1:2 w p title 'numerycznie','' u 1:3 w l title 'analitycznie'
+plot 'File6.txt' u 1:2 w p title 'numerycznie','' u 1:3 w l title 'analitycznie'
 
 set output 'Z02d.jpg'
-plot 'File7' u 1:2 w p title 'numerycznie','' u 1:3 w l title 'analitycznie'
+plot 'File7.txt' u 1:2 w p title 'numerycznie','' u 1:3 w l title 'analitycznie'
 
 set yrange [-4:4]
 set output 'Z02e.jpg'
-plot 'File4' u 1:4 w l title '0.01','File5' u 1:4 w l title '0.1','File6' u 1:4 w l title '0.25','File7' u 1:4 w l title '0.27'
+plot 'File4.txt' u 1:4 w l title '0.01','File5.txt' u 1:4 w l title '0.1','File6.txt' u 1:4 w l title '0.25','File7.txt' u 1:4 w l title '0.27'
 
 set autoscale y
 set output 'Z03a.jpg'
-plot 'File8' u 1:2 w p title 'numerycznie','' u 1:3 w l title 'analitycznie'
+plot 'File8.txt' u 1:2 w p title 'numerycznie','' u 1:3 w l title 'analitycznie'
 
+#Zadanie 3
 set output 'Z03b.jpg'
-plot 'File9' u 1:2 w p title 'numerycznie','' u 1:3 w l title 'analitycznie'
+plot 'File9.txt' u 1:2 w p title 'numerycznie','' u 1:3 w l title 'analitycznie'
 
 set output 'Z03c.jpg'
-plot 'File10' u 1:2 w p title 'numerycznie','' u 1:3 w l title 'analitycznie'
+plot 'File10.txt' u 1:2 w p title 'numerycznie','' u 1:3 w l title 'analitycznie'
 
 set output 'Z03d.jpg'
-plot 'File11' u 1:2 w p title 'numerycznie','' u 1:3 w l title 'analitycznie'
+plot 'File11.txt' u 1:2 w p title 'numerycznie','' u 1:3 w l title 'analitycznie'
 
 set output 'Z03e.jpg'
-plot 'File12' u 1:2 w p title 'numerycznie','' u 1:3 w l title 'analitycznie'
+plot 'File12.txt' u 1:2 w p title 'numerycznie','' u 1:3 w l title 'analitycznie'
 
 set yrange [-4:4]
 set output 'Z03f.jpg'
-plot 'File8' u 1:4 w l title '0.01','File9' u 1:4 w l title '0.1','File10' u 1:4 w l title '0.25','File11' u 1:4 w l title '0.27','File12' u 1:4 w l title '0.27'
+plot 'File8.txt' u 1:4 w l title '0.01','File9.txt' u 1:4 w l title '0.1','File10.txt' u 1:4 w l title '0.25','File11.txt' u 1:4 w l title '0.27','File12.txt' u 1:4 w l title '0.27'
+
+#Zadanie 4
 
 set autoscale y
 set output 'Z04a.jpg'
-plot 'File13' u 1:2 w lp title 'numerycznie','' u 1:3 w l title 'analitycznie'
+plot 'File13.txt' u 1:2 w lp title 'numerycznie','' u 1:3 w l title 'analitycznie'
+
+set output 'Z04b.jpg'
+plot 'File14.txt' u 1:2 w lp title 'numerycznie','' u 1:3 w l title 'analitycznie'
+
+set output 'Z04c.jpg'
+plot 'File15.txt' u 1:2 w lp title 'numerycznie','' u 1:3 w l title 'analitycznie'
+
+#Zadanie 5
+
+set output 'Z05a.jpg'
+plot 'File16.txt' u 1:2 w lp title 'numerycznie','' u 1:3 w l title 'analitycznie'
+
+set output 'Z06a.jpg'
+plot 'File17.txt' u 1:2 w lp title 'numerycznie'
+
+set output 'Z07a.jpg'
+plot 'File18.txt' u 1:2 w lp title 'numerycznie'
+
+set output 'Z08a.jpg'
+plot 'File19.txt' u 1:2 w lp title 'numerycznie','' u 1:3 w l title 'analitycznie'
+
+set output 'Z08b.jpg'
+plot 'File19.txt' u 1:4 w lp title 'błąd'
--- lab2/lab02.py
+++ lab2/lab02.py
@@ -30,12 +30,13 @@ def complicateEuera2(function, ran, dt, t0):
 def iterationEuelera(function, ran, dt, t0):
     u = [t0]
     for x in ran:
-        u.append(t0 + function(0.01, u[-1], 0.01) * 0.01)
+        u.append(t0 + function(dt, u[-1], dt) * dt)
     return u
 
-def newton(function,ran,dt,t0,n):
+
+def newton(function, ran, dt, t0, n):
     u = [t0]
-    umi=[t0]
+    umi = [t0]
     for x in ran:
         u.append(t0 + function(0.01, u[-1], 0.01) * 0.01)
     return u
@@ -49,6 +50,41 @@ def trapez(function, ran, dt, t0):
     return u
 
 
+def newtonIter(function, ran, dt, t0):
+    u = [t0]
+    umi = [t0]
+    for x in ran:
+        u.append(u[-1] - (u[-1] - t0 - dt * function(dt, u[-1], dt)) / (1 - pochodna1(function, dt, u[-1], dt, 0.1)))
+    return u
+
+def newtonIter3(function, ran, dt, t0):
+    u = [t0]
+    umi = [t0]
+    for x in ran:
+        u.append(u[-1] - (u[-1] - t0 - dt * function(dt, u[-1], dt)) / (1 - pochodna2(function, dt, u[-1], dt, 0.1)))
+    return u
+
+
+def newtonIter2(function, ran, dt, t0, n, du):
+    u = [t0]
+    for x in ran:
+        umi = [t0]
+        for b in range(0, n):
+            umi.append(
+                umi[-1] - (umi[-1] - u[-1] - dt / 2 * (function(x + dt, umi[-1], dt) + function(x, u[-1], dt))) / (
+                    1 - dt / 2 * pochodna2(function, x, u[-1], dt, du)))
+        u.append(umi[-1])
+    return u
+
+
+def pochodna1(function, t, u, dt, du):
+    return (function(t, u + du, dt) - function(t, u, dt)) / du
+
+
+def pochodna2(function, t, u, dt, du):
+    return (function(t, u + du, dt) - function(t, u - du, dt)) / (2 * du)
+
+
 #równania różniczkowe
 def function1(t, u, dt):
     return 32 * t - 4
@@ -60,6 +96,10 @@ def function2(t, u, dt):
 
 def function3(t, u, dt):
     return (u + 128 * (t ** 2) * dt + 56 * dt) / (1 + 8 * dt)
+
+
+def function6(t, u, dt):
+    return u * (u - 1) * (u - 2)
 
 
 #rozwiązanie funkcji
@@ -79,14 +119,23 @@ def lab02Z1(fileName, method, function, functionRoz, start, end, dt, u0):
         for x, y, z in zip(ran, u, dok):
             file1.write("{0} {1} {2} {3}\n".format(x, y, z, z - y))
 
-def lab02Z4(fileName,method,function,functionRoz,dt,u0,n):
+
+def lab02Z4(fileName, method, function, functionRoz, dt, u0, n):
     with open(fileName, "w") as file1:
-        ran = range(0,n)
+        ran = range(0, n)
         u = method(function, ran, dt, u0)
         dok = [functionRoz(dt, 0) for t in ran]
         for x, y, z in zip(ran, u, dok):
             file1.write("{0} {1} {2} {3}\n".format(x, y, z, z - y))
 
+
+def lab02Z8(fileName, method, function, functionRoz, start, koniec, krok, u0, iloscIteracji, du):
+    with open(fileName, "w") as fp:
+        ran = drange(start, koniec, krok)
+        u = method(function, ran, krok, u0, iloscIteracji, du)
+        dok = [functionRoz(t, 0) for t in ran]
+        for x, y, z in zip(ran, u, dok):
+            fp.write("{0} {1} {2} {3}\n".format(x, y, z, z - y))
 
 
 def drange(start, stop, step):
@@ -101,24 +150,39 @@ def drange(start, stop, step):
 def main():
     # Zadanie 1
     methodList = (simpleEuera, complicateEuera, trapez)
-    fileNames = ("File1", "File2", "File3")
+    fileNames = ("File1.txt", "File2.txt", "File3.txt")
     for file, method in zip(fileNames, methodList):
         lab02Z1(file, method, function1, function1Roz, 0, 60, 1.5, 7.5)
 
     # Zadanie 2
-    fileNames2 = ("File4", "File5", "File6", "File7")
+    fileNames2 = ("File4.txt", "File5.txt", "File6.txt", "File7.txt")
     steps = (0.01, 0.1, 0.25, 0.27)
 
     for file, step in zip(fileNames2, steps):
         lab02Z1(file, simpleEuera, function2, function2Roz, 0, 10, step, 9.5)
 
     # Zadanie 3
-    fileNames3 = ("File8", "File9", "File10", "File11", "File12")
+    fileNames3 = ("File8.txt", "File9.txt", "File10.txt", "File11.txt", "File12.txt")
     steps3 = (0.01, 0.1, 0.25, 0.27, 1.33)
     for file, step in zip(fileNames3, steps3):
         lab02Z1(file, complicateEuera2, function3, function2Roz, 0, 10, step, 9.5)
     #Zadanie 4
-    lab02Z4("File13", iterationEuelera, function2, function2Roz, 0.01, 9.5,5)
+    fileNames4 = ("File13.txt", "File14.txt", "File15.txt")
+    steps4 = (0.01, 0.125, 0.13)
+    for file, step in zip(fileNames4, steps4):
+        lab02Z4(file, iterationEuelera, function2, function2Roz, step, 9.5, 5)
+
+    #Zadanie 5
+    lab02Z4("File16.txt", newtonIter, function2, function2Roz, 1.33, 9.5, 15)
+
+    #Zadanie 6
+    lab02Z4("File17.txt", newtonIter, function6, function2Roz,1, 1.9, 15)
+
+    #Zadanie 7
+    lab02Z4("File18.txt", newtonIter3, function6, function2Roz,1, 1.9, 15)
+
+    #Zadanie 8
+    lab02Z8("File19.txt", newtonIter2, function2, function2Roz, 0, 30, 0.2, 9.5, 5, 0.1)
 
 
 main()
