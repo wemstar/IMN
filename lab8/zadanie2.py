5dd8dab6d66a372ed03f70e7bc00eadcc7b50b4c
wemstar <sylwestermacura@gmail.com>
2014-05-04 23:51:58 +0200

    Poprawka pętli

--- lab8/zadanie2.py
+++ lab8/zadanie2.py
@@ -19,13 +19,21 @@ def zadanie2(matrix):
         """
     weave.inline(code,['matrix','m','n','dokladny','potencjal'],support_code=support)
 
-    with open("Zadanie2Poch.txt","w") as fp1,open("Zadanie2Dok.txt","w") as fp2:
+    with open("Zadanie2Poch.txt","w") as fp1:
         (m, n) = matrix.shape
         for i in range(m):
             for j in range(n):
                 fp1.write("{0:0.20f} {1:0.20f} {2:0.20f} \n".format(i - 60, j - 60, potencjal[i][j]))
-                fp2.write("{0:0.20f} {1:0.20f} {2:0.20f} \n".format(i - 60, j - 60, dokladny[i][j]))
+
             fp1.write("\n")
+
+    with open("Zadanie2Dok.txt","w") as fp2:
+        (m, n) = matrix.shape
+        for i in range(m):
+            for j in range(n):
+
+                fp2.write("{0:0.20f} {1:0.20f} {2:0.20f} \n".format(i - 60, j - 60, dokladny[i][j]))
+
             fp2.write("\n")
 support="""
 double potencjalFun(double i,double j)
